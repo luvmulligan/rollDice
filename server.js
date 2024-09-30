6eb67ab3498cd3d8ac4431cb9a89cf83190e88d2
Lucía Velarde Mulligan <luciavelardemulligan@gmail.com>
2024-09-30 17:39:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Servir los archivos de la carpeta dist de Angular
-app.use(express.static(__dirname + '/dist/roll-dice'));
+app.use(express.static(__dirname + '/dist'));
 
 // Redirigir todas las rutas al archivo index.html de Angular
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/roll-dice/browser/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Iniciar la app en el puerto que Heroku indique
